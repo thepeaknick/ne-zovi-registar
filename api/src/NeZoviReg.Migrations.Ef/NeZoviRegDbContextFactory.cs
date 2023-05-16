c84c73ffe298e37abe6e43b907fcbb955528e808
draganml <dragan.mladenovic@endava.com>
2023-05-16 18:06:35 +0200

    docker

--- api/src/NeZoviReg.Migrations.Ef/NeZoviRegDbContextFactory.cs
+++ api/src/NeZoviReg.Migrations.Ef/NeZoviRegDbContextFactory.cs
@@ -11,8 +11,7 @@ public class NeZoviRegDbContextFactory : IDesignTimeDbContextFactory<NeZoviRegDa
     {
         IConfigurationRoot configuration = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional:false, true)
-            .AddJsonFile("appsettings.my.json", optional:true, true)
+            .AddJsonFile("migrations.json", optional:false, true)
             .Build();
 
         var connectionString = configuration.GetConnectionString("SqlServerDatabase");
