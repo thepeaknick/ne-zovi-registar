756e90da8cc3fe3ea64eb7422294d78242830d09
draganml <dragan.mladenovic@endava.com>
2023-05-16 18:09:35 +0200

    no message

--- api/Run.ps1
+++
@@ -1,9 +0,0 @@
-cd d:\Projects\nezovireg.git_docker\api\
-
-# for test
-#docker rm  sql_server2022 --force 
-#docker rm  nezovireg_api --force 
-
-docker-compose up -d
-#docker exec nezovireg_api NeZoviReg.Migrations.Ef.exe
-dotnet run \src\NeZoviReg.Migrations.Ef\bin\Release\net7.0\NeZoviReg.Migrations.Ef.exe
