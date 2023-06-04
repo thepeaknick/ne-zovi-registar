be1b5504e5962a946ce9bd415c3425e3454e1d52
Dragan Basta <bastadragan@gmail.com>
2023-06-04 12:25:17 +0200

    Added deafult navigation to admin dashboard home.

--- angular/src/app/views/pages/login/login.component.ts
+++ angular/src/app/views/pages/login/login.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  token: string | undefined;
 
-  token: string|undefined;
-
-  constructor() {
+  constructor(private router: Router) {
     this.token = undefined;
   }
 
+  goDashboardHome(): void {
+    const navigationDetails: string[] = ['/admin'];
+    this.router.navigate(navigationDetails);
+
+    //TODO: Swiftch case ADMIN, MERCHANT, OPERATOR
+  }
 }
