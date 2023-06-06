f9b81ac5148c0e3a9060677631602676db5bb665
Dragan Basta <bastadragan@gmail.com>
2023-06-06 16:26:51 +0200

    CORS fixed for local development.

--- angular/src/app/domain/services/authentication.service.ts
+++ angular/src/app/domain/services/authentication.service.ts
@@ -6,86 +6,82 @@ import { RefreshTokenResultDto, TokenResult } from '../model/schemas';
 
 @Injectable({ providedIn: 'root' })
 export class AuthenticationService {
-    constructor(
-        private config: AppConfiguration, 
-        private router: Router,
-        private http: HttpClient) 
-    {
-    }
+  constructor(
+    private config: AppConfiguration,
+    private router: Router,
+    private http: HttpClient
+  ) {}
 
-    login(username: string, password: string) {
-        return this.http
-            .post<TokenResult>(
-                `${this.config.apiUrl}${this.config.apiLoginUrl}`, 
-                { username, password })
-            .subscribe((token: TokenResult) => {
-                AuthenticationService.Token = token;
-                this.startRefreshTokenTimer();
-            });
-    }
+  login(username: string, password: string) {
+    return this.http
+      .post<TokenResult>(`${this.config.apiUrl}${this.config.apiLoginUrl}`, {
+        username,
+        password,
+      })
+      .subscribe((token: TokenResult) => {
+        AuthenticationService.Token = token;
+        this.startRefreshTokenTimer();
+        console.log('DONE');
+      });
+  }
 
-    logout() {
-        this.http
-            .post<any>(`${this.config.apiUrl}${this.config.apiLogoutUrl}`, {})
-            .subscribe();
+  logout() {
+    this.http
+      .post<any>(`${this.config.apiUrl}${this.config.apiLogoutUrl}`, {})
+      .subscribe();
 
-        this.stopRefreshTokenTimer();
-        AuthenticationService.Token = null;
-        this.router.navigate([`${this.config.loginPage}`]);
-    }
+    this.stopRefreshTokenTimer();
+    AuthenticationService.Token = null;
+    this.router.navigate([`${this.config.loginPage}`]);
+  }
 
-    refreshToken() {
-        let token: TokenResult = AuthenticationService.Token;
-        return this.http
-            .post<RefreshTokenResultDto>(
-                `${this.config.apiUrl}${this.config.apiRefreshTokenUrl}`, 
-                { 
-                    accessToken: token.accessToken, 
-                    refreshToken: token.refreshToken.tokenString 
-                }
-            )
-            .subscribe((token: RefreshTokenResultDto) => {
-                AuthenticationService.Token = {
-                    accessToken: token.accessToken,
-                    refreshToken: token.refreshToken
-                };
-                this.startRefreshTokenTimer();
-            });
-    }
+  refreshToken() {
+    let token: TokenResult = AuthenticationService.Token;
+    return this.http
+      .post<RefreshTokenResultDto>(
+        `${this.config.apiUrl}${this.config.apiRefreshTokenUrl}`,
+        {
+          accessToken: token.accessToken,
+          refreshToken: token.refreshToken.tokenString,
+        }
+      )
+      .subscribe((token: RefreshTokenResultDto) => {
+        AuthenticationService.Token = {
+          accessToken: token.accessToken,
+          refreshToken: token.refreshToken,
+        };
+        this.startRefreshTokenTimer();
+      });
+  }
 
+  // token store
 
-    // token store
+  static _TOKEN_ITEM = '_TOKEN_ITEM';
+  static set Token(token: TokenResult | null | string) {
+    if (token)
+      if (typeof token === 'string')
+        localStorage.setItem(this._TOKEN_ITEM, token);
+      else localStorage.setItem(this._TOKEN_ITEM, JSON.stringify(token));
+    else localStorage.removeItem(this._TOKEN_ITEM);
+  }
 
-    static _TOKEN_ITEM = "_TOKEN_ITEM";
-    static set Token(token: TokenResult | null | string) {
-        if(token)
-            if(typeof token === 'string')
-                localStorage.setItem(this._TOKEN_ITEM, token);
-            else
-                localStorage.setItem(this._TOKEN_ITEM, JSON.stringify(token));
-        else
-            localStorage.removeItem(this._TOKEN_ITEM);
-    }
+  static get Token(): TokenResult {
+    return JSON.parse(localStorage.getItem(this._TOKEN_ITEM) ?? '{}');
+  }
 
-    static get Token() : TokenResult {
-        return JSON.parse(localStorage.getItem(this._TOKEN_ITEM) ?? "{}");
-    }
+  // helper methods
 
+  private refreshTokenTimeout: any;
 
+  private startRefreshTokenTimer() {
+    let tokens: TokenResult = AuthenticationService.Token;
+    // set a timeout to refresh the token a minute before it expires
+    const expires = new Date(tokens.refreshToken.expireAt);
+    const timeout = expires.getTime() - Date.now() - 60 * 1000;
+    this.refreshTokenTimeout = setTimeout(() => this.refreshToken(), timeout);
+  }
 
-    // helper methods
-
-    private refreshTokenTimeout: any;
-
-    private startRefreshTokenTimer() {
-        let tokens: TokenResult = AuthenticationService.Token;
-        // set a timeout to refresh the token a minute before it expires
-        const expires = new Date(tokens.refreshToken.expireAt);
-        const timeout = expires.getTime() - Date.now() - (60 * 1000);
-        this.refreshTokenTimeout = setTimeout(() => this.refreshToken(), timeout);
-    }
-
-    private stopRefreshTokenTimer() {
-        clearTimeout(this.refreshTokenTimeout);
-    }
+  private stopRefreshTokenTimer() {
+    clearTimeout(this.refreshTokenTimeout);
+  }
 }
