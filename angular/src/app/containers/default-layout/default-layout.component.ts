2165a193122a78c44a6fb10222ceea1bb3aec3d0
Dragan Basta <bastadragan@gmail.com>
2023-06-06 14:53:59 +0200

    Sidemenu implemented.

--- angular/src/app/containers/default-layout/default-layout.component.ts
+++ angular/src/app/containers/default-layout/default-layout.component.ts
@@ -5,9 +5,9 @@ import { navItems } from './_nav';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './default-layout.component.html',
+  styleUrls: ['./default-layout.component.scss'],
 })
 export class DefaultLayoutComponent {
-
   public navItems = navItems;
 
   public perfectScrollbarConfig = {
@@ -15,4 +15,14 @@ export class DefaultLayoutComponent {
   };
 
   constructor() {}
+
+  vars = {
+    '--cui-sidebar-bg': '#133F85',
+    '--cui-sidebar-toggler-bg': 'rgba(0, 0, 21, 0.0)',
+    '--cui-sidebar-brand-bg': 'rgba(0, 0, 21, 0.0)',
+    '--cui-sidebar-header-bg': 'rgba(0, 0, 21, 0.0)',
+    '--cui-sidebar-nav-group-bg': 'rgba(0, 0, 21, 0.0)',
+    '--cui-sidebar-nav-link-active-bg': '#3F6AA3',
+    // '--my-another-css-var': 'red',
+  };
 }
