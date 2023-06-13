d1a63922015eb7dd8823468343e5c0ebaf360b6c
Marko Bubulj <markobubulj@gmail.com>
2023-06-13 10:11:10 +0200

    bla bla

--- api/src/NeZoviReg.WebApi/Controllers/AccountController.cs
+++ api/src/NeZoviReg.WebApi/Controllers/AccountController.cs
@@ -84,7 +84,7 @@ public class AccountController : NeZoviRegBaseController
     /// <returns></returns>
     [HttpGet("forgotpassword/{email:required}")]
     [ProducesResponseType(typeof(string), (int) HttpStatusCode.OK)]
-    [HasPermission(PermissionType.RegUsersOnly | PermissionType.Read)]
+    [AllowAnonymous]
     public async Task<IActionResult> ForgotRegUserPassword(string email, CancellationToken cancellationToken)
     {
         var command = new ForgotPassCommand(email)
@@ -103,7 +103,7 @@ public class AccountController : NeZoviRegBaseController
     /// <returns></returns>
     [HttpPost("forgotpassword")]
     [ProducesResponseType(typeof(bool), (int) HttpStatusCode.OK)]
-    [HasPermission(PermissionType.RegUsersOnly | PermissionType.Read)]
+    [AllowAnonymous]
     public async Task<IActionResult> ResetRegUserPassword([FromBody] ResetRegUserPasswordRequest request,
         CancellationToken cancellationToken)
     {
