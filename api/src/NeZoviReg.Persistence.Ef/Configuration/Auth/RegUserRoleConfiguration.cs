6323bbbd605386d8951ffc6fe38c976f657d50fc
Marko Bubulj <markobubulj@gmail.com>
2023-06-13 15:50:10 +0200

    no message

--- api/src/NeZoviReg.Persistence.Ef/Configuration/Auth/RegUserRoleConfiguration.cs
+++ api/src/NeZoviReg.Persistence.Ef/Configuration/Auth/RegUserRoleConfiguration.cs
@@ -23,12 +23,12 @@ public class RegUserRoleConfiguration : IEntityTypeConfiguration<RegUserRole>
         builder.HasOne(sc => sc.Role)
             .WithMany(s => s.RegUserRoles)
             .HasForeignKey(sc => sc.RoleId)
-            .OnDelete(DeleteBehavior.ClientNoAction);
+            .OnDelete(DeleteBehavior.Cascade);
 
         builder.HasOne(sc => sc.RegUser)
             .WithMany(s => s.RegUserRoles)
             .HasForeignKey(sc => sc.RegUserId)
-            .OnDelete(DeleteBehavior.ClientNoAction);
+            .OnDelete(DeleteBehavior.Cascade);
 
         builder.HasData(Create(1, RoleType.Admin));
 
