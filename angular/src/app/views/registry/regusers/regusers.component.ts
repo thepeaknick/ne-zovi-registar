fc86db30be49a66ffbb3f75af98c5576ad66d0c4
Petar Ristic <petar.r016@gmail.com>
2023-06-14 22:37:29 +0200

    RegUsers pagination

--- angular/src/app/views/registry/regusers/regusers.component.ts
+++ angular/src/app/views/registry/regusers/regusers.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
+import { PaginationComponent } from '@coreui/angular';
 import { RegUserDto, RoleType } from 'src/app/domain/model/schemas';
 import { RegUserService } from 'src/app/domain/services/reguser.service';
 
@@ -13,13 +14,25 @@ export class RegUsersComponent implements OnInit {
 
   @Input() public regUsers: RegUserDto[] = [];
 
+  showInTableUsers: RegUserDto[] = [];
+
+  itemsPerPage = 10;
+  currentPage = 0;
+  totalPagesNumber = 0;
+  pages: number[] = [];
+
   ngOnInit(): void {
     this.regUserService.getRegUsers(RoleType.Obveznik).subscribe({
       next: (regUsers: RegUserDto[]) =>
         (this.regUsers = regUsers instanceof HttpErrorResponse ? [] : regUsers),
-      complete: () => this.addRegUsers(),
+      complete: () => { 
+          console.log(" this.regUsers.length ",  this.regUsers.length); 
+          this.totalPagesNumber = Math.trunc(this.regUsers.length / this.itemsPerPage) + 1;
+          this.setPage(1);
+          // this.addRegUsers()
+      },
     });
-    // 2023-06-11T12:58:03.3910839
+    this.currentPage = 1;
   }
 
   addRegUsers() {
@@ -37,4 +50,20 @@ export class RegUsersComponent implements OnInit {
       });
     }
   }
+
+  setPage(page: number) {
+    this.currentPage = page;
+    this.showInTableUsers = this.regUsers.slice( (page - 1) * this.itemsPerPage, page * this.itemsPerPage)
+  }
+
+  setItemPerPage(num: number) {
+    this.itemsPerPage = num;
+    this.setPage(this.currentPage)
+    this.totalPagesNumber = Math.trunc(this.regUsers.length / this.itemsPerPage) + 1;
+  }
+
+  vars = {
+    '--cui-pagination-active-bg': '#321fdb !important'
+  };
+
 }
