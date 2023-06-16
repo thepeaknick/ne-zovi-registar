8d738710b0e24c2d943f00fba9a664719de79399
Marko Bubulj <markobubulj@gmail.com>
2023-06-16 18:41:03 +0200

    no message

--- api/src/NeZoviReg.Persistence.Ef/Configuration/Domain/UserConfiguration.cs
+++ api/src/NeZoviReg.Persistence.Ef/Configuration/Domain/UserConfiguration.cs
@@ -35,5 +35,7 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
             .WithMany()
             .HasForeignKey(kp => kp.OperatorId)
             .OnDelete(DeleteBehavior.Cascade);
+        
+        builder.HasIndex(x => x.PhoneNumber).IsUnique();
     }
 }
