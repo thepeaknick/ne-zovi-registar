00cd7d783e05a5b28fb6ec31440897c6160e4b45
Marko Bubulj <markobubulj@gmail.com>
2023-06-16 21:52:14 +0200

    no message

--- api/src/NeZoviReg.WebApi/Extensions/Startup.cs
+++ api/src/NeZoviReg.WebApi/Extensions/Startup.cs
@@ -118,7 +118,7 @@ public static class Startup
             options.AddPolicy("any",
                 policy =>
                 {
-                    policy.WithOrigins("http://158.180.60.234:*")
+                    policy.AllowAnyOrigin()
                         .AllowAnyHeader()
                         .AllowAnyMethod();
                 });
