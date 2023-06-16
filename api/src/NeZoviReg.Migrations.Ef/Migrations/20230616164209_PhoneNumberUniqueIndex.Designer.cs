0e1a47532249d5e63c361c3b760fc349bd18fe33
Marko Bubulj <markobubulj@gmail.com>
2023-06-16 18:42:32 +0200

    no message

--- api/src/NeZoviReg.Migrations.Ef/Migrations/20230616164209_PhoneNumberUniqueIndex.Designer.cs
+++ api/src/NeZoviReg.Migrations.Ef/Migrations/20230616164209_PhoneNumberUniqueIndex.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NeZoviReg.Persistence.Ef;
 
@@ -11,9 +12,11 @@ using NeZoviReg.Persistence.Ef;
 namespace NeZoviReg.Migrations.Ef.Migrations
 {
     [DbContext(typeof(NeZoviRegDataContext))]
-    partial class NeZoviRegDataContextModelSnapshot : ModelSnapshot
+    [Migration("20230616164209_PhoneNumberUniqueIndex")]
+    partial class PhoneNumberUniqueIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
