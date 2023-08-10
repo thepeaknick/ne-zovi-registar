af3d09df9d965bf57da3a4455930c7571ac6f5a3
Marko Bubulj <markobubulj@gmail.com>
2023-08-11 00:44:47 +0200

    Add Statistika

--- api/src/NeZoviReg.Abstractions/Messaging/Domain/Model/User/UserInfoDto.cs
+++ api/src/NeZoviReg.Abstractions/Messaging/Domain/Model/User/UserInfoDto.cs
@@ -3,37 +3,47 @@
 /// <summary>
 ///  Informacije o registrovanom broju telefona.
 /// </summary>
-/// <param name="PhoneNumber"> Broj telefona</param>
-/// <param name="Active">Postoji u registru</param>
-/// <param name="RegisteredOn">Datum registracije</param>
-/// <param name="RemovedOn">Datum brisanja iz registra</param>
-/// <param name="Operator">Naziv operatora</param>
 public sealed record UserInfoDto
 {
+    /// <summary>
+    /// Broj telefona
+    /// </summary>
     public required string PhoneNumber
     {
         get;
         init;
     }
     
+    /// <summary>
+    /// Postoji u registru
+    /// </summary>
     public required bool Active
     {
         get;
         init;
     }
     
+    /// <summary>
+    /// Datum registracije
+    /// </summary>
     public required DateTime RegisteredOn
     {
         get;
         init;
     }
     
+    /// <summary>
+    /// Datum brisanja iz registra
+    /// </summary>
     public required DateTime? RemovedOn
     {
         get;
         init;
     }
     
+    /// <summary>
+    /// Naziv operatora
+    /// </summary>
     public required string Operator
     {
         get;
