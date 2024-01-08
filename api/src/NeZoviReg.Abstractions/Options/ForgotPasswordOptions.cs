06af09d501fc0972019458ce7b50c5a73c6438aa
Marko Bubulj <markobubulj@gmail.com>
2024-01-08 17:39:31 +0100

    chnage template

--- api/src/NeZoviReg.Abstractions/Options/ForgotPasswordOptions.cs
+++ api/src/NeZoviReg.Abstractions/Options/ForgotPasswordOptions.cs
@@ -7,22 +7,10 @@ public class ForgotPasswordOptions
     public const string SectionName = "ForgotPassword";
     
     [Required]
-    public string? SmtpServer { get; set; }
-
-    [Required]
-    public int Port { get; set; }
-
-    [Required]
     public string? EmailFrom { get; set; }
 
     [Required]
     public string? Subject { get; set; }
-    
-    [Required]
-    public string? Username { get; set; }
-
-    [Required]
-    public string? Password { get; set; }
     
     [Required]
     public string? HtmlTemplatePath { get; set; }
