31e95ccd6533af62ddfb272917da7f06462bd8a7
Marko Bubulj <markobubulj@gmail.com>
2024-03-14 19:43:03 +0100

    Apr integratoin

--- api/src/NeZoviReg.Application/Services/RegUser/CreateRegUserCommandHandler.cs
+++ api/src/NeZoviReg.Application/Services/RegUser/CreateRegUserCommandHandler.cs
@@ -40,7 +40,7 @@ internal sealed class CreateRegUserCommandHandler : ICommandHandler<CreateRegUse
 
     public async Task<Result<RegUserDto>> Handle(CreateRegUserCommand command, CancellationToken cancellationToken)
     {
-        await _aprWebClient.GetAprBusinessEntityAsync(command.RegNumber, cancellationToken);
+        //await _aprWebClient.GetAprBusinessEntityAsync(command.RegNumber, cancellationToken);
         
         var regUser = new Domain.Model.Domain.RegUser(command.CompanyName, command.UserName)
             .WithAddress(command.Address)
