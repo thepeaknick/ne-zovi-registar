2cbe46da7b280b4619a8870506571a823b8913ba
Marko Bubulj <markobubulj@gmail.com>
2024-03-27 16:50:41 +0100

    APR integracija DOO and Preduzetnici

--- api/src/NeZoviReg.Abstractions/Infrastructure/WebClient/IAprWebClient.cs
+++ api/src/NeZoviReg.Abstractions/Infrastructure/WebClient/IAprWebClient.cs
@@ -4,7 +4,5 @@ namespace NeZoviReg.Abstractions.Infrastructure.WebClient;
 
 public interface IAprWebClient
 {
-    Task<List<AprBusinessEntity>> GetAprBusinessEntitiesAsync(string regNumber, CancellationToken cancellationToken = default);
-
     Task<AprBusinessEntity?> GetAprBusinessEntityAsync(string regNumber, CancellationToken cancellationToken = default);
 }
--- api/src/NeZoviReg.WebClient/Apr/Mapping/AprNeZoviRegMappingProfile.cs
+++ api/src/NeZoviReg.WebClient/Apr/Mapping/AprNeZoviRegMappingProfile.cs
@@ -8,53 +8,92 @@ public class AprNeZoviRegMappingProfile : Profile
 {
     public AprNeZoviRegMappingProfile()
     {
-        CreateMap<PrivredniSubjekat, AprBusinessEntity>()
+        CreateMap<(PrivredniSubjekat AprData, PrivredniSubjekatMaticniBrojTip Tip), AprBusinessEntity>()
             .ForMember(d => d.RegNumber, o =>
             {
-                o.Condition(s => s.grupa.Any());
+                o.Condition(s => s.Tip == PrivredniSubjekatMaticniBrojTip.Item1 && s.AprData.grupa.Any());
                 o.MapFrom((s, _) =>
-                    s.grupa.FirstOrDefault(x => x.id == "1001")?.podatak.FirstOrDefault(p => p.naziv == "MaticniBroj")
+                    s.AprData.grupa.FirstOrDefault(x => x.id == "1001")?.podatak.FirstOrDefault(p => p.naziv == "MaticniBroj")
                         ?.vrednost);
+                o.Condition(s => s.Tip == PrivredniSubjekatMaticniBrojTip.Item2 && s.AprData.grupa.Any());
+                o.MapFrom((s, _) =>
+                    s.AprData.grupa.FirstOrDefault(x => x.id == "1041")?.podatak.FirstOrDefault(p => p.naziv == "MaticniBroj")
+                        ?.vrednost);
+                
             })
             .ForMember(d => d.CompanyName, o =>
             {
-                o.Condition(s => s.grupa.Any());
+                o.Condition(s => s.Tip == PrivredniSubjekatMaticniBrojTip.Item1 && s.AprData.grupa.Any());
                 o.MapFrom((s, _) =>
-                    s.grupa.FirstOrDefault(x => x.id == "1006")?.podatak
+                    s.AprData.grupa.FirstOrDefault(x => x.id == "1006")?.podatak
+                        .FirstOrDefault(p => p.naziv == "SkracenoPoslovnoIme")?.vrednost);
+                o.Condition(s => s.Tip == PrivredniSubjekatMaticniBrojTip.Item2 && s.AprData.grupa.Any());
+                o.MapFrom((s, _) =>
+                    s.AprData.grupa.FirstOrDefault(x => x.id == "1046")?.podatak
                         .FirstOrDefault(p => p.naziv == "SkracenoPoslovnoIme")?.vrednost);
             })
             .ForMember(d => d.FirstName, o =>
             {
-                o.Condition(s => s.grupa.Any());
+                o.Condition(s => s.Tip == PrivredniSubjekatMaticniBrojTip.Item1 && s.AprData.grupa.Any());
                 o.MapFrom((s, _) =>
-                    s.grupa.FirstOrDefault(x => x.id == "1027")?.podatak.FirstOrDefault(p => p.naziv == "Ime")
+                    s.AprData.grupa.FirstOrDefault(x => x.id == "1027")?.podatak.FirstOrDefault(p => p.naziv == "Ime")
                         ?.vrednost);
+                o.Condition(s => s.Tip == PrivredniSubjekatMaticniBrojTip.Item2 && s.AprData.grupa.Any());
+                o.MapFrom((s, _) =>
+                    {
+                        var vrednost = s.AprData.grupa.FirstOrDefault(x => x.id == "1048")?.podatak
+                            .FirstOrDefault(p => p.naziv == "ImeOsnivaca")
+                            ?.vrednost;
+
+                        return vrednost?[..(vrednost?.IndexOf(" ") ?? 0)];
+                    });
             })
             .ForMember(d => d.LastName, o =>
             {
-                o.Condition(s => s.grupa.Any());
+                o.Condition(s => s.Tip == PrivredniSubjekatMaticniBrojTip.Item1 && s.AprData.grupa.Any());
                 o.MapFrom((s, _) =>
-                    s.grupa.FirstOrDefault(x => x.id == "1027")?.podatak.FirstOrDefault(p => p.naziv == "Prezime")
+                    s.AprData.grupa.FirstOrDefault(x => x.id == "1027")?.podatak.FirstOrDefault(p => p.naziv == "Prezime")
                         ?.vrednost);
+                
+                o.Condition(s => s.Tip == PrivredniSubjekatMaticniBrojTip.Item2 && s.AprData.grupa.Any());
+                o.MapFrom((s, _) =>
+                {
+                    var vrednost = s.AprData.grupa.FirstOrDefault(x => x.id == "1048")?.podatak
+                        .FirstOrDefault(p => p.naziv == "ImeOsnivaca")
+                        ?.vrednost;
+
+                    return vrednost?[(vrednost.IndexOf(" ", StringComparison.Ordinal) + 1)..];
+                });
             })
             .ForMember(d => d.TaxNumber, o =>
             {
-                o.Condition(s => s.grupa.Any());
+                o.Condition(s => s.Tip == PrivredniSubjekatMaticniBrojTip.Item1 && s.AprData.grupa.Any());
                 o.MapFrom((s, _) =>
-                    s.grupa.FirstOrDefault(x => x.id == "1017")?.podatak.FirstOrDefault(p => p.naziv == "PIB")
+                    s.AprData.grupa.FirstOrDefault(x => x.id == "1017")?.podatak.FirstOrDefault(p => p.naziv == "PIB")
+                        ?.vrednost);
+                o.Condition(s => s.Tip == PrivredniSubjekatMaticniBrojTip.Item2 && s.AprData.grupa.Any());
+                o.MapFrom((s, _) =>
+                    s.AprData.grupa.FirstOrDefault(x => x.id == "1058")?.podatak.FirstOrDefault(p => p.naziv == "PIB")
                         ?.vrednost);
             })
             .ForMember(d => d.Email, o =>
             {
-                o.Condition(s => s.grupa.Any());
+                o.Condition(s => s.Tip == PrivredniSubjekatMaticniBrojTip.Item1 && s.AprData.grupa.Any());
                 o.MapFrom((s, _) =>
-                    s.grupa.FirstOrDefault(x => x.id == "1013")?.podatak.FirstOrDefault(p => p.naziv == "EMailAdresa")
+                    s.AprData.grupa.FirstOrDefault(x => x.id == "1013")?.podatak.FirstOrDefault(p => p.naziv == "EMailAdresa")
+                        ?.vrednost);
+                o.Condition(s => s.Tip == PrivredniSubjekatMaticniBrojTip.Item2 && s.AprData.grupa.Any());
+                o.MapFrom((s, _) =>
+                    s.AprData.grupa.FirstOrDefault(x => x.id == "1054")?.podatak.FirstOrDefault(p => p.naziv == "EMailAdresa")
                         ?.vrednost);
             })
             .ForMember(d => d.TheAddress, o =>
             {
-                o.Condition(s => s.grupa.Any());
-                o.MapFrom((s, _) => s.grupa.FirstOrDefault(x => x.id == "1011"));
+                o.Condition(s => s.Tip == PrivredniSubjekatMaticniBrojTip.Item1 && s.AprData.grupa.Any());
+                o.MapFrom((s, _) => s.AprData.grupa.FirstOrDefault(x => x.id == "1011"));
+                
+                o.Condition(s => s.Tip == PrivredniSubjekatMaticniBrojTip.Item2 && s.AprData.grupa.Any());
+                o.MapFrom((s, _) => s.AprData.grupa.FirstOrDefault(x => x.id == "1052"));
             });
 
         CreateMap<Grupa, Address>()
